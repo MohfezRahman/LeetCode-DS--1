9c68b2f0e6cbfcdbceb60b75b57f5c5aecbee83d
Mohfez Rahman <mohfezrahman@gmail.com>
2021-11-27 02:10:08 -0500

    Formatted

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,12 +1,12 @@
-var containsDuplicate=function(nums){
-    const visited ={}
-    for(var i=0; i<nums.length;i++){
-        const num =nums[i]
-        if(visited[num]!==undefined ){
-            return true
-        }
-        visited[num]=i
+var containsDuplicate = function (nums) {
+  const visited = {};
+  for (var i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (visited[num] !== undefined) {
+      return true;
     }
-    return false
-}
-console.log(containsDuplicate([1,2,3,0]))
+    visited[num] = i;
+  }
+  return false;
+};
+console.log(containsDuplicate([1, 2, 3, 0]));
